f8041ca3f2b1761ae3cd0c51a7468554c872148e
ouichouani <ouichouaniabdlhakim@gmal.com>
2024-11-25 23:01:39 +0100

    add a js code to start addin the tasks

---
+++ list.js
@@ -0,0 +1,22 @@
+
+
+
+const input_task = document.getElementById("input_task") ;
+const button = document.getElementById('add') ;
+
+button.addEventListener('click' , () => {
+    console.log(input_task.value) ;
+    input_task.value = '' ;
+})
+
+
+
+
+
+// Object.assign(input_task , {style : "background-color : red"})
+
+
+
+
+
+
