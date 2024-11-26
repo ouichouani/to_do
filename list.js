3cd8979af28ed3667d52a8a00b08fd50935269ba
ouichouani <ouichouaniabdlhakim@gmal.com>
2024-11-27 00:37:06 +0100

    add and delet a task from the list

--- list.js
+++ list.js
@@ -1,22 +1,62 @@
 
 
 
-const input_task = document.getElementById("input_task") ;
-const button = document.getElementById('add') ;
+const input_task = document.getElementById("input_task");
+const button = document.getElementById('add');
+const container_tasks = document.getElementsByClassName("container_tasks")[0]
+const container_tasks_done = document.getElementsByClassName("container_tasks_done")[0]
+const task = document.getElementsByClassName('task')
+const checkbox = document.getElementsByClassName('checkbox')
+const task_text = document.getElementsByClassName('task_text')
 
-button.addEventListener('click' , () => {
-    console.log(input_task.value) ;
-    input_task.value = '' ;
-})
-
-
-
-
-
-// Object.assign(input_task , {style : "background-color : red"})
+let id_counter = 0
 
 
+function add_to_container_tasks() {
+    button.addEventListener('click', () => {
+        container_tasks.innerHTML += `
+                <div class="task">
+                        <div class="checkbox"></div>
+                        <p class='task_text'>${input_task.value}</p>
+                    </div>`
+        input_task.value = '';
+        mark_task()
+    })
+}
+
+
+function romove_task_from_tasks_list(i) {
+    for (j = i; j < container_tasks.childElementCount; j++) {
+        try {
+            task_text[j].innerHTML = task_text[j + 1].innerHTML
+        } catch {
+            continue;
+        }
+    }
+    task_text[container_tasks.childElementCount - 1].textContent = ''
+    try {
+            task_text[container_tasks.childElementCount - 1].parentElement.remove()
+    } catch {
+        console.log('catch')
+    }
+
+}
 
 
 
 
+function mark_task() {
+    for (let i = 0; i < container_tasks.childElementCount; i++) {
+
+        checkbox[i].addEventListener('click', function () {
+            romove_task_from_tasks_list(i)
+        })
+    }
+}
+
+
+
+
+
+add_to_container_tasks()
+mark_task()
